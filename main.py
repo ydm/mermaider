85d0b6b0bc9a04ad5cb60ccab29a70bc87c8e51a
Yordan Miladinov <jordanmiladinov@gmail.com>
2023-03-12 03:05:17 +0200

    main: either read from argv[1] or stdin

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 #!/usr/bin/env python3
 
 import json
+import sys
 from mermaider.visitor import to_mermaid
 
 
 def main():
-    artifacts = json.loads(input())
+    if len(sys.argv) >= 2:
+        with open(sys.argv[1], 'r', encoding='utf-8') as f:
+            artifacts = json.load(f)
+    else:
+        artifacts = json.loads(input())
     to_mermaid(artifacts)
 
 
